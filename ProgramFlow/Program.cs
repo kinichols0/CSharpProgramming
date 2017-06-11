acfe4e8cd60e896eac108f2ba2de26bafee93489
Kelvin Nichols <kinichols0@yahoo.com>
2017-06-11 09:14:54 -0400

    Console app run changes

--- ProgramFlow/Program.cs
+++ ProgramFlow/Program.cs
@@ -12,24 +12,51 @@ namespace ProgramFlow
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Program started...{0}\n\n", DateTime.Now.ToString());
+            Console.WriteLine("Program started...{0}\n", DateTime.Now.ToString());
 
-            /* Run demo of basic Parallel For method execution */
-            //ParallelDemo.RunParallelForBasic();
-            //ParallelDemo.ParallelForAdditionRun();
-            //ParallelDemo.ParallelForEach();
-            //ParallelDemo.ParallelInvokeRun();
+            // prompt available process to run
+            Console.WriteLine("Which demo do you want to run?");
+            Console.WriteLine("[1] - ParallelFor Basic Run\n[2] - ParallelFor Concurrent Addition Run\n" +
+                "[3] - ParallelForEach demo\n[4] - ParallelInvoke demo");
+            Console.WriteLine("[5] - Task demo\n[6] - Multiple Task Demo\n[7] - Continuation Task demo\n[8] - Cancel task after 10 seconds demo.");
+            Console.WriteLine("[9] - Thread demo\n[10] - Background Worker demo\n[11] - Threadpool demo\n[12] - Cancel background worker demo.");
 
-            /* Task demos */
-            // TaskDemo.Run();
-            // TaskDemo.RunTasks();
-            // TaskDemo.RunTasksWithContinution();
-            // TaskDemo.CancelAfterTenSecondsDemo();
-
-            /* Thread demos */
-            // ThreadingDemo.BasicRun();
-            // ThreadingDemo.RunBackagroundWorker();
-            ThreadingDemo.ThreadPoolDemoRun();
+            // read the input and run the corresponding process
+            string key = Console.ReadLine();
+            if (int.TryParse(key, out int demoNum))
+            {
+                switch (demoNum)
+                {
+                    case 1: ParallelDemo.RunParallelForBasic();
+                        break;
+                    case 2: ParallelDemo.ParallelForAdditionRun();
+                        break;
+                    case 3: ParallelDemo.ParallelForEach();
+                        break;
+                    case 4: ParallelDemo.ParallelInvokeRun();
+                        break;
+                    case 5: TaskDemo.Run();
+                        break;
+                    case 6: TaskDemo.RunTasks();
+                        break;
+                    case 7: TaskDemo.RunTasksWithContinution();
+                        break;
+                    case 8: TaskDemo.CancelAfterTenSecondsDemo();
+                        break;
+                    case 9: ThreadingDemo.BasicRun();
+                        break;
+                    case 10: ThreadingDemo.RunBackagroundWorker();
+                        break;
+                    case 11: ThreadingDemo.ThreadPoolDemoRun();
+                        break;
+                    case 12: ThreadingDemo.BackgroundWorkerCancellationDemo();
+                        break;
+                    default: Console.WriteLine("Could not find a process corresponding to {0} to run. Program will exit now.", demoNum);
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("Not valid input. Program will exit now.");
 
             Console.WriteLine("Program ended...,{0}", DateTime.Now.ToString());
 
--- ProgramFlow/TaskDemos/ThreadingDemo.cs
+++ ProgramFlow/TaskDemos/ThreadingDemo.cs
@@ -11,21 +11,38 @@ namespace ProgramFlow.TaskDemos
 {
     public class ThreadingDemo
     {
+        #region Basic Thread Demo
+
+        private static AutoResetEvent autoEvent;
+
         public static void BasicRun()
         {
-            Console.WriteLine("Threading basic demo run started...\n\n");
+            Console.WriteLine("Threading basic demo run started...");
 
+            autoEvent = new AutoResetEvent(false);
+
+            // create and start the thread
             Thread thread = new Thread(BasicRunMethod);
             thread.Start();
 
-            Console.WriteLine("Ended threading basic run...\n\n");
+            // wait for the one thread to end
+            autoEvent.WaitOne();
+
+            Console.WriteLine("Ended threading basic run...\n");
         }
 
         private static void BasicRunMethod()
         {
+            // run the thread process
             Console.WriteLine("Basic thread process writing to the console...");
+
+            // signal that the one thread ended
+            autoEvent.Set();
         }
 
+        #endregion
+
+        #region Basic Background Worker Demo
         public static void RunBackagroundWorker()
         {
             Console.WriteLine("Started background worker demo...\n\n");
@@ -54,7 +71,9 @@ namespace ProgramFlow.TaskDemos
             double area = (double)e.Result;
             Console.WriteLine("The area is {0}", area.ToString());
         }
+        #endregion
 
+        #region Threadpool demo
         public static void ThreadPoolDemoRun()
         {
             Console.WriteLine("Started Threadpool demo run...");
@@ -117,5 +136,62 @@ namespace ProgramFlow.TaskDemos
                 return number;
             return FibonacciCalc(number - 1) + FibonacciCalc(number - 2);
         }
+        #endregion
+
+        #region Background worker cancellation demo
+
+        public static void BackgroundWorkerCancellationDemo()
+        {
+            Console.WriteLine("Started background worker cancellation demo...");
+
+            // initialize the worker. set worker flags to support cancellation and report progress
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = true;
+            worker.WorkerReportsProgress = true;
+
+            // hook up event handlers
+            worker.DoWork += BackgroundWorker_DoWork;
+            worker.RunWorkerCompleted += BackgroundWorker_WorkCompleted;
+
+            // start the worker
+            worker.RunWorkerAsync();
+
+            var cancelKey = Console.ReadLine();
+            if (cancelKey == "cancel" && worker.IsBusy)
+                worker.CancelAsync();
+            else
+                Console.WriteLine("Process has been completed");
+
+            Console.WriteLine("Ended background worker cancellation demo...");
+        }
+
+        private static void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Console.WriteLine("Two minute process has begun. \nEnter 'cancel' to end the process sooner.");
+            var worker = (BackgroundWorker)sender;
+
+            // Process that runs for two minutes
+            for(int i = 0; i < 120; i++)
+            {
+                // if cancelled break.
+                if (worker.CancellationPending)
+                    break;
+
+                Thread.Sleep(1000);
+                Console.WriteLine("{0} seconds passed", i + 1);
+            }
+        }
+
+        private static void BackgroundWorker_WorkCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+                Console.WriteLine("Error: {0}", e.Error.Message);
+            else if (e.Cancelled)
+                Console.WriteLine("Process canceled");
+            else
+                Console.WriteLine("Process finished");
+        }
+
+        #endregion
     }
 }
