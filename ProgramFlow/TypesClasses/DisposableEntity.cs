7e484b3a3a2a78db1880fdf84b10fb2573172ada
Kelvin Nichols <kinichols0@yahoo.com>
2017-06-21 21:27:11 -0400

    IDisposable implementation

---
+++ ProgramFlow/TypesClasses/DisposableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming.TypesClasses
+{
+    public class DisposableEntity : IDisposable
+    {
+        /// <summary>
+        /// Release unmanaged resources
+        /// </summary>
+        public void Dispose()
+        {
+
+        }
+    }
+}
